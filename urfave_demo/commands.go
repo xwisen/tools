0cfe1cb2581fd8fb139ae48b270e97d61ad9e8b1
xwisen <1031649164@qq.com>
2017-02-14 14:52:21 +0800

    Modify urfave demo

--- urfave_demo/commands.go
+++ urfave_demo/commands.go
@@ -8,12 +8,20 @@ package deploy
 //        Author: xwisen 1031649164@qq.com
 //   Description: ---
 //        Create: 2017-02-13 15:28:42
-// Last Modified: 2017-02-14 10:39:38
+// Last Modified: 2017-02-14 14:24:07
 //***********************************************
 
 import (
+	//"encoding/json"
+	"bufio"
+	"errors"
 	"fmt"
 	"github.com/urfave/cli"
+	"io"
+	//"io/ioutil"
+	//"k8s.io/client-go/pkg/api/v1"
+	//"k8s.io/client-go/rest"
+	"os"
 )
 
 var Commands = []cli.Command{
@@ -88,15 +96,57 @@ var Commands = []cli.Command{
 
 func CreateDeployment(ctx *cli.Context) error {
 	fmt.Println("create a deployment")
+	//获取全局Flag
+	fmt.Printf("global flag names :%s,%s\n", ctx.GlobalFlagNames(), ctx.GlobalString("kc"))
+	//获取子命令Args
 	fmt.Println("Arg is ", ctx.NArg(), ctx.Args())
 	for i := 0; i < ctx.NArg(); i++ {
 		fmt.Printf("Arg %d is %s\n", i, ctx.Args().Get(i))
 	}
-
+	//获取子命令Flag
 	fmt.Println("Flag is ", ctx.NumFlags(), ctx.FlagNames())
 	for _, flagName := range ctx.FlagNames() {
 		fmt.Printf("Flag: %s, Value :%s\n", flagName, ctx.String(flagName))
 	}
+	fd, err := os.OpenFile(ctx.String("file"), os.O_RDONLY, os.ModeTemporary)
+	defer fd.Close()
+	if err != nil {
+		return errors.New(fmt.Sprintf("Open file %s error !", ctx.String("file")))
+	}
+	//使用io/ioutil读取文件内容,据说这种速度最快
+	//content, err := ioutil.ReadAll(fd)
+	//if err != nil {
+	//	return errors.New(fmt.Sprintf("ioutil readall from %s error !", ctx.String("file")))
+	//}
+	//fmt.Printf("`io/ioutil` content is :\n%s\n", string(content))
+	//使用原生read和buf
+	chunks := make([]byte, 1024, 1024)
+	buf := make([]byte, 1024)
+	//for {
+	//	n, err := fd.Read(buf)
+	//	if err != nil && err != io.EOF {
+	//		return errors.New(fmt.Sprintf("error is %s", err))
+	//	}
+	//	if n == 0 {
+	//		break
+	//	}
+	//	chunks = append(chunks, buf[:n]...)
+	//}
+	//fmt.Printf("`read/buf` content is :\n%s\n", string(chunks))
+	//使用bufio读取
+	r := bufio.NewReader(fd)
+	for {
+		n, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return errors.New(fmt.Sprintf("error is %s", err))
+		}
+		if n == 0 {
+			break
+		}
+		chunks = append(chunks, buf[:n]...)
+
+	}
+	fmt.Printf("`bufio` content is :\n%s\n", string(chunks))
 	return nil
 }
 
--- urfave_demo/flags.go
+++ urfave_demo/flags.go
@@ -8,10 +8,16 @@ package deploy
 //        Author: xwisen 1031649164@qq.com
 //   Description: ---
 //        Create: 2017-02-13 15:26:02
-// Last Modified: 2017-02-13 16:28:02
+// Last Modified: 2017-02-14 12:44:50
 //***********************************************
 import (
 	"github.com/urfave/cli"
 )
 
-var Flags = []cli.Flag{}
+var Flags = []cli.Flag{
+	cli.StringFlag{
+		Name:   "kubeconfig,kc",
+		Value:  "./kubeconfig",
+		EnvVar: "KUBE_CONFIG",
+	},
+}
