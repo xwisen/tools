35e4d65f5a06fc2702f3be21e34966de7fd04493
xwisen <1031649164@qq.com>
2017-03-17 17:19:23 +0800

    Modify fmt

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tools
 1. [reg_tools.sh](/reg_tools.sh) 镜像仓库查询/删除脚本
-2. [json/yaml](/json-yaml.sh) json/yaml转换工具
+2. [json_yaml.sh](/json_yaml.sh) json/yaml转换工具
--- reg_tools.sh
+++ reg_tools.sh
@@ -102,28 +102,3 @@ print("delete image : "+"'${1}'" + " succeed !")
 '
 }
 
-#eg: yamltojson test.yaml test.json
-function yamltojson () {
-	python -c '
-import sys, yaml, json;
-json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)
-'< $1 > $2
-	if [[ $? -ne 0  ]];then
-		echo ">>>>>>convert failed !"
-	else
-		echo ">>>>>>convert succeed ! input is : $1, output is : $2"
-	fi
-}
-
-#eg: jsontoyaml test.json test.yaml
-function jsontoyaml () {
-	python -c '
-import sys, yaml, json;
-yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)
-'< $1 > $2
-	if [[ $? -ne 0  ]];then
-		echo ">>>>>>convert failed !"
-	else
-		echo ">>>>>>convert succeed ! input is : $1, output is : $2"
-	fi
-}
